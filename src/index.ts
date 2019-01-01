f7bd1df7e28c508b4b7f204c73d3a845daffb8b4
yasaichi <yasaichi@users.noreply.github.com>
2019-01-01 18:01:32 +0900

    Reimplement the plugin as Class
    in order that it's able to accept options

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { logger } from 'hypernova/server';
 
-export default {
-  onError(_context: any, err: Error) {
+class ServerStackTracePlugin {
+  constructor(options?: any) {
+    // Do nothing for now
+  }
+
+  public onError(context: any, err: Error) {
     if (err.stack !== undefined) {
       logger.error(err.stack);
     }
   }
-};
+}
+
+export = ServerStackTracePlugin;
